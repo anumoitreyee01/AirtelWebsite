a4602d47733dde8fd34fc67dfa16da888194e861
anumoitreyee01 <76536964+anumoitreyee01@users.noreply.github.com>
2021-09-20 21:09:35 +0530

    image red

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,12 +3,14 @@ import Container from '@material-ui/core/Container';
 
 
 import SwipeableTextMobileStepper from '../Components/Movies';
+import { red } from '@mui/material/colors';
 
 
 export default function Home() {
   return (
     <React.Fragment>
         <Container fixed>
+          <img src={red} alt="red" />
         
              
             
