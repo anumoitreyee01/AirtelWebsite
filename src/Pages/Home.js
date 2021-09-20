6c3263f707192df57591c9fd36025eb12fc29493
anumoitreyee01 <76536964+anumoitreyee01@users.noreply.github.com>
2021-09-20 20:03:38 +0530

    3 changes made- updated button

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
 import DatePickers from '../Components/DateTimePicker';
+import ContainedButtons from '../Components/Button';
 
 export default function Home() {
   return (
     <React.Fragment>
         <Container fixed>
+             <ContainedButtons/>
             <p>
                 Welcome to your web application. Make something beautiful!
             </p>
