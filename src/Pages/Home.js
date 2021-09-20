e57b1d8df7cafc7fd289cff3e07fe0bf55cba740
anumoitreyee01 <76536964+anumoitreyee01@users.noreply.github.com>
2021-09-20 19:18:16 +0530

    fullwidthtab

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,15 +4,17 @@ import Container from '@material-ui/core/Container';
 
 import SwipeableTextMobileStepper from '../Components/Movies';
 import { red } from '@mui/material/colors';
+import FullWidthTabs from '../Components/FullWidthTab';
 
 
 export default function Home() {
   return (
     <React.Fragment>
         <Container fixed>
-          <img src={red} alt="red" />
+          <img src={red} alt="red"/>
+
         
-             
+            <FullWidthTabs/>
             
             
             <SwipeableTextMobileStepper/>
